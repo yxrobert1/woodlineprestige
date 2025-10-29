2d221dbcd3327838edf392e90802f40b09b4ad10
yxrobert1 <robertcosma1809@gmail.com>
2025-10-29 15:30:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+let lastScroll = 0;
+const header = document.getElementById('header');
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll) {
+        header.classList.add('hidden');
+    } else {
+        header.classList.remove('hidden');
+    }
+
+    lastScroll = currentScroll;
+});
